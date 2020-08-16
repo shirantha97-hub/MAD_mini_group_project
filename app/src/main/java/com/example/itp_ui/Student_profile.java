0f20a45837462fb56f1504a80c2d333b2a73f252
Srilal Fonseka <srilal.fonseka67@gmail.com>
2020-08-16 19:40:10 +0530

    Initial commit

---
+++ app/src/main/java/com/example/itp_ui/Student_profile.java
@@ -0,0 +1,14 @@
+package com.example.itp_ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Student_profile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_profile);
+    }
+}
